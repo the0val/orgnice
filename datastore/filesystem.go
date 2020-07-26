3447ce798c97058ac308e8ea11f42012a327cab5
David Agardh <agardh.david@gmail.com>
2020-07-26 13:21:47 +0200

    Rename filesystem to datastore

---
+++ datastore/filesystem.go
@@ -0,0 +1,23 @@
+package datastore
+
+import (
+	"os"
+)
+
+var orgniceRoot string
+
+func createFolder(path string) error {
+	err := os.Mkdir(path, os.ModeDir+os.ModePerm)
+	if err != nil {
+		fileInfo, e := os.Stat(path)
+		fileMode := fileInfo.Mode()
+		if e == nil && fileMode.IsDir() {
+			if fileMode.Perm()&0700 != 0700 {
+				return os.Chmod(path, fileMode|0700)
+			}
+			return nil
+		}
+		return err
+	}
+	return nil
+}
