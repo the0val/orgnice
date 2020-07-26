55bed9d9e40be1913fbafd69aaee2e679a882a86
David Agardh <agardh.david@gmail.com>
2020-07-26 13:18:34 +0200

    Removed InitDir
    It's a better fit for the application layer.

--- filesystem/filesystem.go
+++ filesystem/filesystem.go
@@ -1,26 +1,10 @@
 package filesystem
 
 import (
-	"log"
 	"os"
-	"path"
 )
 
 var orgniceRoot string
-
-// InitDir creates the neccesary folder structure
-// for the apps datastorage.
-func InitDir() {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatalf("Unable to find users home dir\n%v", err)
-	}
-	orgniceRoot = path.Join(home, ".orgnice")
-	err = createFolder(orgniceRoot)
-	if err != nil {
-		log.Fatalf("Unable to initialize folder %v\t%v", orgniceRoot, err)
-	}
-}
 
 func createFolder(path string) error {
 	err := os.Mkdir(path, os.ModeDir+os.ModePerm)
